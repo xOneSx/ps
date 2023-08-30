f4bbd04d5230bca7fb90603db7a72fc3fa433a27
OneS <136712971+xOneSx@users.noreply.github.com>
2023-08-30 21:09:44 +0300

    10.4

--- webproj/advertisements/advertisements/urls.py
+++ webproj/advertisements/advertisements/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('myauth/', include('app_auth.urls')),
     path('', include('app_advertisements.urls')),
 ]
 
--- webproj/advertisements/app_advertisements/forms.py
+++ webproj/advertisements/app_advertisements/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-
+'''
 class AdvertisementForm(forms.Form):
     title = forms.CharField(max_length=64, widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}))
     description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control form-control-lg'}))
@@ -10,3 +10,27 @@ class AdvertisementForm(forms.Form):
         attrs={'class': 'form-check-input'}), required=False
     )
     image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control form-control-lg'}))
+'''
+
+from django.core.exceptions import ValidationError
+from .models import Advertisement
+
+
+class AdvertisementForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs['class'] = 'form-control form-control-lg'
+        self.fields['description'].widget.attrs['class'] = 'form-control form-control-lg'
+        self.fields['image'].widget.attrs['class'] = 'form-control form-control-lg'
+        self.fields['price'].widget.attrs['class'] = 'form-control form-control-lg'
+        self.fields['negotiable'].widget.attrs['class'] = 'form-check-input'
+
+    class Meta:
+        model = Advertisement
+        fields = ("title", "description", "image", "price", "negotiable")
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if title.startswith('?'):
+            raise ValidationError('Заголовок не может начинаться с вопросительного знака.')
+        return title
